e1df6dbbb0fcce659a67b9a13da8154a5ca616fb
JOAO VICTOR ALVES DOS SANTOS <hyperlobe2@gmail.com>
2025-09-17 23:46:31 -0300

    First Commit

---
+++ src/users/users.controller.ts
@@ -0,0 +1,45 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+
+@Controller('users')
+export class UsersController {
+  /*
+    GET /users or /users?role=value
+    GET /users/:id
+    POST /users
+    PATCH /users/:id
+    DELETE /users/:id
+ */
+  @Get()
+  findAll(@Query('role') role?: 'INTERN' | 'ADMIN' | 'ENG') {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
+}
